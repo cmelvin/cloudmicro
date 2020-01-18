47987a4ab024180d3573fc18049a18ee213a9697
Cephas Melvin <cmel777@Cephass-MacBook-Pro.local>
2020-01-18 07:37:16 +0000

    Working Eureka naming service for service discovery and service registration

--- pub-service/src/main/java/com/cloudmicro/microservices/pubservice/PubServiceApplication.java
+++ pub-service/src/main/java/com/cloudmicro/microservices/pubservice/PubServiceApplication.java
@@ -2,11 +2,14 @@ package com.cloudmicro.microservices.pubservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 @EnableFeignClients("com.cloudmicro.microservices.pubservice")
+@EnableDiscoveryClient
 public class PubServiceApplication {
 
 	public static void main(String[] args) {
